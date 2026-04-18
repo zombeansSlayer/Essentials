3bb1392c81ea6672239c8045076ae3e05ade1e72
zombeansSlayer <134235713+zombeansSlayer@users.noreply.github.com>
2026-04-17 22:45:12 -0600

    Fixed character controller and revised door mechanics

--- DAGV1700/AdventureGame/Assets/Scripts/PlayerManager.cs
+++ DAGV1700/AdventureGame/Assets/Scripts/PlayerManager.cs
@@ -5,8 +5,8 @@ using System.Collections;
 
 public class PlayerManager : MonoBehaviour
 {
-    public GameObject playerDeath;
     public SimpleFloatData[] uiValues; // Index 0 is the haunt, index 1 is the health
+    public AudioSource deathSound;
     public float healthDrain;
     public float moveSpeed;
     public float jumpStrength;
@@ -17,6 +17,7 @@ public class PlayerManager : MonoBehaviour
     private Rigidbody rb;
     private bool dead = false;
     private bool jump = false;
+    private bool checkSuccess = false;
 
 
     void Awake()
@@ -28,19 +29,19 @@ public class PlayerManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        // MOVEMENT
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) rb.linearVelocity = new Vector3(moveSpeed * -1, rb.linearVelocity.y, 0);
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) rb.linearVelocity = new Vector3(moveSpeed, rb.linearVelocity.y, 0);
-        else rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
-        chrAnim.SetFloat("Run", Mathf.Abs(rb.linearVelocity.x));
-        // JUMP
-        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && jump == true) 
-        {
-            jump = false;
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpStrength, rb.linearVelocity.z);
-        }
-        if (jump == false) rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y - (gravity * 0.05f), 0);
-        chrAnim.SetBool("Jump", !jump);
+            // MOVEMENT
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) rb.linearVelocity = new Vector3(moveSpeed * -1, rb.linearVelocity.y, 0);
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) rb.linearVelocity = new Vector3(moveSpeed, rb.linearVelocity.y, 0);
+            else rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
+            chrAnim.SetFloat("Run", Mathf.Abs(rb.linearVelocity.x));
+            // JUMP
+            if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && jump == true)
+            {
+                jump = false;
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpStrength, rb.linearVelocity.z);
+            }
+            if (jump == false) rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y - (gravity * 0.05f), 0);
+            chrAnim.SetBool("Jump", !jump);
         // HEALTH
         if (uiValues[0].value > 0 && uiValues[1].value > 0) 
             uiValues[1].value -= (uiValues[0].value * healthDrain); // If the player is haunted and their health isn't 0, reduce it
@@ -48,11 +49,9 @@ public class PlayerManager : MonoBehaviour
             uiValues[1].value += healthDrain * 10; // Otherwise, increase it
         uiValues[1].value = Math.Clamp(uiValues[1].value, 0, 1); // Cap the health
         // DEATH
-        if (uiValues[1].value <= 0) 
+        if (uiValues[1].value <= 0)
         {
-            dead = true; // The player DIES
-            playerDeath.SetActive(true);
-            ResetLevel();
+            Death();
         }
     }
 
@@ -71,13 +70,31 @@ public class PlayerManager : MonoBehaviour
     {
         if (other.gameObject.layer == 6) jump = false;
     }
+
+    public void checkHaunt(int hauntRequirement)
+    {
+        if (uiValues[0].value >= hauntRequirement) checkSuccess = true;
+    }
+    public void breakDoor(GameObject door)
+    {
+        if (checkSuccess == true) door.GetComponent<Animator>().SetTrigger("Open");
+        checkSuccess = false;
+    }
+
     private IEnumerator Reset()
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    public void ResetLevel() 
+    public void Death()
     {
+        dead = true; // The player DIES
+        gameObject.transform.GetChild(1).gameObject.SetActive(true);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        deathSound.Play();
+        jump = false;
+        gravity = Mathf.Abs(gravity) * -1;
         StartCoroutine(Reset());
     }
 }
